89b4e915148f86bfe47f3d9ef093edf17e719a2e
wcallaghan <90016258+wcallaghan@users.noreply.github.com>
2021-11-24 09:24:07 -0500

    Update artifacts.js

--- artifacts.js
+++ artifacts.js
@@ -1,6 +1,7 @@
-document.getElementById("links").innerHTML= "<a href = 'FIS_artifact.html'>Fast Inverse Square Root</a>";
+document.getElementById("links").innerHTML= 
+  "<a href = 'FIS_artifact.html'>Fast Inverse Square Root</a>";
 
-document.getElementById("node").innerHTML = "
-<h1>About me</h1>
-<p>An aspiring developer finishing his Bachelor's degree at Kent State University. Creative problem solver that loves taking on new challenges.</p>
-";
+document.getElementById("node").innerHTML = 
+  "<h1>About me</h1>
+   <p>An aspiring developer finishing his Bachelor's degree at Kent State University. Creative problem solver that loves taking on new challenges.</p>
+   ";
