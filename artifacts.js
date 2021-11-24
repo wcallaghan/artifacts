5b8264a7b656ec550f3b3bba5c2f78c1aafb001a
wcallaghan <90016258+wcallaghan@users.noreply.github.com>
2021-11-24 08:15:13 -0500

    Create artifacts.js

---
+++ artifacts.js
@@ -0,0 +1,9 @@
+documemnt.getElementById("links").InnerHTML= "
+<a href= "index.html">Home</a>
+<a href = "FIS_artifact.html">Fast Inverse Square Root</a>
+";
+
+document.getElementById("node").InnerHTML = "
+<h1>About me</h1>
+<p>An aspiring developer finishing his Bachelor's degree at Kent State University. Creative problem solver that loves taking on new challenges.</p>
+";
