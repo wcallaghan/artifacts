1834fb2ab8c9ce8fe95638c15a67de533730e935
wcallaghan <90016258+wcallaghan@users.noreply.github.com>
2021-11-24 09:45:28 -0500

    Update FIS_artifact.js

--- FIS_artifact.js
+++ FIS_artifact.js
@@ -1,9 +1,5 @@
-document.getElementById("FIS_nav").innerHTML = "
-<a href = 'index.html'>Home</a>
-";
-
-document.getElementById("FIS_div").innerHTML = "
-<h1>Practical Application of Fast Inverse Square Root Method<h1>
+document.getElementById("FIS_div").innerHTML = 
+  "<h1>Practical Application of Fast Inverse Square Root Method<h1>
   <p>It does not take a lot of experimenting with computers to come to the conclusion that they process data very quickly. Unforunately not all data transformations are fast.
     Notably, division is a slow process when compared to multiplication and addition; many common programming languages also hold a math library which contains useful methods
     Java and many others have a method to solve for square root, math.sqrt(a), which is helpful but regrettably slow. When presented with 1/√x coding this in Java is simple
@@ -11,5 +7,4 @@ document.getElementById("FIS_div").innerHTML = "
 <h2>Why should we care?</h2>
 <p>The difference between the processes are only milliseconds, so why would it make sense to take extra effort to save fractions of a second? The answer lies in scope;
   in a vacummn the difference is miniscule, but when writing a function or method that will take thousands or tens of thousands of parameters these small differences add up very
-  quickly.</p>
-";
+  quickly.</p>";
