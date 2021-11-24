121fe464e9a4e45e1ce2a47d01411fb4a5a89500
wcallaghan <90016258+wcallaghan@users.noreply.github.com>
2021-11-24 09:14:25 -0500

    Update artifacts.js

--- artifacts.js
+++ artifacts.js
@@ -1,6 +1,4 @@
-documemnt.getElementById("links").innerHTML= "
-<a href = "FIS_artifact.html">Fast Inverse Square Root</a>
-";
+documemnt.getElementById("links").innerHTML= "<a href = "FIS_artifact.html">Fast Inverse Square Root</a>";
 
 document.getElementById("node").innerHTML = "
 <h1>About me</h1>
